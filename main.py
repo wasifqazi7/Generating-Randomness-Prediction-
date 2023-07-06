b63c0b9d7cd45949dc645a926423aee568b2cc1c
wasifqazi <wasifqazi@icloud.com>
2023-07-06 14:31:28 +0530

    couting 0's and 1's following a triad

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ while length_of_string < MIN_STR_LENGTH:
     print('Current data length is', length_of_string, end='')
     print(',', symbol_left, 'symbols left.')
     
+print('Final data String:')
+print(main_string)
+
+final_string_splitted = [main_string[symbol:symbol+4] for symbol in range(0, len(main_string))] 
+triads = ["000", "001", "010", "011", "100", "101", "110", "111"]
+zeros = [0, 0, 0, 0, 0, 0, 0, 0]
+ones = [0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in range(len(triads)):
+    for j in range(len(final_string_splitted)):
+        if final_string_splitted[j].startswith(triads[i]):
+            if final_string_splitted[j][-1] == '0':
+                zeros[i] += 1
+            elif final_string_splitted[j][-1] == '1':
+                ones[i] += 1
+
+for i in range(len(triads)):
+    print(f'{triads[i]}: {zeros[i]},{ones[i]}')
